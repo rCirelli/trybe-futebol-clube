76973d1243f0beaba5922b3112939aef92c3a775
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-10-26 20:12:56 -0300

    feat: create new match endpoint

--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 const matchController = new MatchController();
 
 router.get('/', (req, res) => matchController.getAll(req, res));
+router.post('/', (req, res) => matchController.create(req, res));
 
 export default router;
