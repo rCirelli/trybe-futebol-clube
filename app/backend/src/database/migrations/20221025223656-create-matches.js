6624726736d38082af419b0d7224e38478724152
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-10-25 21:13:59 -0300

    feat: create Matches migration and seeder

---
+++ app/backend/src/database/migrations/20221025223656-create-matches.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      homeTeam: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'home_team',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'teams',
+          hey: 'id',
+        },
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'home_team_goals',
+      },
+      awayTeam: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'away_team',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'teams',
+          hey: 'id',
+        },
+      },
+      awayTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'away_team_goals',
+      },
+      inProgress: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        field: 'in_progress',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
