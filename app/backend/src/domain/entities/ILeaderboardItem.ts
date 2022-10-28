08a71dd6c4d2a0cba10f78d683f4052f94a55376
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-10-27 23:42:59 -0300

    feat: create Leaderboard item interface

---
+++ app/backend/src/domain/entities/ILeaderboardItem.ts
@@ -0,0 +1,14 @@
+import Team from './Team';
+
+export default interface ILeaderboardItem {
+  name: Team['teamName'];
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  efficiency: number;
+}
